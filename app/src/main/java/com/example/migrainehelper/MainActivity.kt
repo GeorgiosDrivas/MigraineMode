be056114c4a7ecd666df78373a0dbb20a1aca12b
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-01-15 12:48:48 +0200

    Separate Brightness functionality into own class

--- app/src/main/java/com/example/migrainehelper/MainActivity.kt
+++ app/src/main/java/com/example/migrainehelper/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.migrainehelper
 
 import android.annotation.SuppressLint
+import android.content.ContentResolver
 import android.content.Context
 import android.os.Bundle
 import android.provider.Settings
-import android.view.View
-import android.widget.Button
 import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -18,41 +18,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val sw1: Switch? = findViewById(R.id.switch1)
+        val brightnessCls = brightness();
+        val currentBr = brightnessCls.getCurrentBrightness(this);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val sw1: Switch? = findViewById(R.id.switch1)
-
         sw1?.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                Brightness(this);
+                brightnessCls.brightnessFn(this, 10);
             } else {
+                brightnessCls.brightnessFn(this, currentBr);
                 println("Not checked")
             }
         }
-
-
-    }
-}
-
-fun Brightness(ts: Context){
-    if (Settings.System.canWrite(ts)) {
-        println("Checked")
-        Settings.System.putInt(
-            ts.contentResolver,
-            Settings.System.SCREEN_BRIGHTNESS_MODE,
-            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
-        )
-
-        Settings.System.putInt(
-            ts.contentResolver,
-            Settings.System.SCREEN_BRIGHTNESS,
-            15
-        )
-    } else {
-        println("Permission not granted. Please enable WRITE_SETTINGS.")
     }
 }
---
+++ app/src/main/java/com/example/migrainehelper/brightness.kt
@@ -0,0 +1,44 @@
+package com.example.migrainehelper
+
+import android.content.ContentResolver
+import android.content.Context
+import android.provider.Settings
+
+class brightness {
+
+    public fun brightnessFn(ts: Context, v: Int){
+        if (Settings.System.canWrite(ts)) {
+            println("Checked")
+            Settings.System.putInt(
+                ts.contentResolver,
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+            )
+
+            Settings.System.putInt(
+                ts.contentResolver,
+                Settings.System.SCREEN_BRIGHTNESS,
+                v
+            )
+        } else {
+            println("Permission not granted. Please enable WRITE_SETTINGS.")
+        }
+    }
+
+    public fun getCurrentBrightness(context: Context): Int{
+        val contentResolver: ContentResolver = context.contentResolver
+
+        Settings.System.putInt(
+            contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS_MODE,
+            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+        )
+
+        val br = Settings.System.getInt(
+            contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS
+        )
+
+        return br;
+    }
+}
