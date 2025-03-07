59b1f89c562efd8e2feb4ad77b7536161246ebf4
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-03-07 12:09:07 +0200

    Remove notifications manager permissions

--- app/src/main/java/com/example/migrainehelper/AudioMode.kt
+++ app/src/main/java/com/example/migrainehelper/AudioMode.kt
@@ -1,28 +1,18 @@
-import android.app.NotificationManager
 import android.content.Context
-import android.content.Intent
 import android.media.AudioManager
-import android.provider.Settings
 
 class AudioMode(private val context: Context) {
 
     fun audioMode(mode: Int) {
-        val notificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (!notificationManager.isNotificationPolicyAccessGranted) {
-            val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
-            context.startActivity(intent)
-        } else {
-            val audioManager =
-                context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-            when (mode) {
-                AudioManager.RINGER_MODE_VIBRATE,
-                AudioManager.RINGER_MODE_NORMAL,
-                AudioManager.RINGER_MODE_SILENT -> {
-                    audioManager.ringerMode = mode
-                }
-                else -> throw IllegalArgumentException("Invalid ringer mode")
-            }        }
+        val audioManager =
+            context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        when (mode) {
+            AudioManager.RINGER_MODE_VIBRATE,
+            AudioManager.RINGER_MODE_NORMAL,
+            AudioManager.RINGER_MODE_SILENT -> {
+                audioManager.ringerMode = mode
+            }
+            else -> throw IllegalArgumentException("Invalid ringer mode")
+        }
     }
 }
